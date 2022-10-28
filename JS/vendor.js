b4e7c579fe45c48cbba5d12e83efdd6033db8827
Patel-Mohammed-Ali <74188800+patel9619ali@users.noreply.github.com>
2022-10-28 19:33:53 +0530

    Update vendor.js

--- JS/vendor.js
+++ JS/vendor.js
@@ -1,48 +1,48 @@
 const account1 = {
-    owner: 'Patel Mohammed Ali',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, 
-    pin: 1111,
-  };
-  
-  const account2 = {
-    owner: 'Mohammed Saif Patel',
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
-  };
-  
-  const account3 = {
-    owner: 'Mohammed Kaif Patel',
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
-  };
-  
-  const account4 = {
-    owner: 'Mohammed Fahad Patel',
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
-  };
-  const account5 = {
-    owner: 'Mohammed Azeem Siddique',
-    movements: [460, 1300, 710, 560, 930],
-    interestRate: 1,
-    pin: 5555,
-  };
-  
+  owner: 'Patel Mohammed Ali',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, 
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Mohammed Saif Patel',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Mohammed Kaif Patel',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Mohammed Fahad Patel',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+const account5 = {
+  owner: 'Mohammed Azeem Siddique',
+  movements: [460, 1300, 710, 560, 930],
+  interestRate: 1,
+  pin: 5555,
+};
+
 const account = [account1,account2,account3,account4,account5];
 let username = function(accountName){
-    let mapAccount = accountName.owner.toLowerCase().split(' ').map(function(initials){
-        return initials[0];
-    });
-   let usernameOfAccount = mapAccount.join('');
-   accountName.username = usernameOfAccount;
-   console.log(accountName)
+  let mapAccount = accountName.owner.toLowerCase().split(' ').map(function(initials){
+      return initials[0];
+  });
+ let usernameOfAccount = mapAccount.join('');
+ accountName.username = usernameOfAccount;
+ console.log(accountName)
 }
 account.forEach(function(allAccount){
-    username(allAccount);
+  username(allAccount);
 });
 
 let requestLoanInput = document.querySelector('.request_loan_input');
@@ -60,43 +60,46 @@ const formattedToday = day + '/' + month + '/' + year;
 let movementsInTheAccount = document.querySelector('.movements_in_the_account');
 console.log(movementsInTheAccount);
 let displayAccountInfo = function(movement){
-    movement.forEach((amount,i) => {
-        const type = amount>0 ? 'Deposited' : `Withdrawed`;
-            let html = `
-            <div class="row movements">
-                            <div class="col-md-4 col-6">
-                                <p class="amount_movement credited_movment">${i+1} ${type}</p>
-                            </div>
-                            <div class="col-md-4 col-6">
-                                <p class="date_movement">${formattedToday}</p>
-                            </div>
-                            <div class="col-md-4 col-12">
-                                <p class="exact_amount">$${Math.abs(amount)}</p>
-                            </div>
-                        </div>
-                        `;
-            movementsInTheAccount.insertAdjacentHTML('afterbegin', `${html}`);
-    });
+  movement.forEach((amount,i) => {
+      const type = amount>0 ? 'Deposited' : `Withdrawed`;
+          let html = `
+          <div class="row movements">
+                          <div class="col-md-4 col-6">
+                              <p class="amount_movement credited_movment">${i+1} ${type}</p>
+                          </div>
+                          <div class="col-md-4 col-6">
+                              <p class="date_movement">${formattedToday}</p>
+                          </div>
+                          <div class="col-md-4 col-12">
+                              <p class="exact_amount">$${Math.abs(amount)}</p>
+                          </div>
+                      </div>
+                      `;
+          movementsInTheAccount.insertAdjacentHTML('afterbegin', `${html}`);
+  });
 }
 
 let loanInput = function(){
-    movement.push(requestLoanInput.value);
-    displayAccountInfo(movement.slice(-1));
+  movement.push(requestLoanInput.value);
+  displayAccountInfo(movement.slice(-1));
 }
 
 
-// let loginUsername = function(){
-//     for (let i = 0; i < account.length; i++) {
-//         const element = account[i];
-//         if((element.username === userNameValue.value) && (element.pin == userPinValue.value)){
-//             appDisplay.classList.remove('d-none');
-//             appDisplay.classList.add('d-block');
-//         }
-//         else{
-//             alert('Please Enter valid Id');
-//         }
-//     }  
-// }
+let loginUsername = function(){
+  let found = false;
+    for (let i = 0; i < account.length; i++) {
+        const element = account[i];
+        if((element.username === userNameValue.value) && (element.pin == userPinValue.value)){
+            appDisplay.classList.remove('d-none');
+            appDisplay.classList.add('d-block');
+            found = true;
+        }
+      } 
+      if(!found){
+        alert('Please Enter valid Id');
+    } 
+       
+}
 // let loginUsername = function(){
 //         if((account.username === userNameValue.value) && (element.pin == userPinValue.value)){
 //             appDisplay.classList.remove('d-none');
